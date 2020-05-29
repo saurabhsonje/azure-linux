37762a4469965765df28a02b58d34f5f69f16606
saurabhsonje <50398982+saurabhsonje@users.noreply.github.com>
2020-05-29 14:07:00 +0530

    Update gitlab.tf

--- terraformAzure/gitlab/gitlab.tf
+++ terraformAzure/gitlab/gitlab.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "GitlabVm" {
     resource_group_name   = data.terraform_remote_state.remote.outputs.rgname
     size                  = "Standard_DS1_v2"
    
-    network_interface_ids = [azurerm_network_interface.nic4.id]
+    network_interface_ids = [azurerm_network_interface.nic3.id]
     os_disk {
        name              = "myOsDisk"
         caching           = "ReadWrite"
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "GitlabVm" {
  
 }
 
-resource "azurerm_network_interface" "nic4" {
+resource "azurerm_network_interface" "nic3" {
   name                = "azurerm-nic"
   location            = data.terraform_remote_state.remote.outputs.rglocation
   resource_group_name = data.terraform_remote_state.remote.outputs.rgname
@@ -59,13 +59,13 @@ resource "azurerm_network_interface" "nic4" {
     name                          = "internal"
     subnet_id                     = data.terraform_remote_state.remote.outputs.subnetId
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id  = azurerm_public_ip.publicip4.id
+    public_ip_address_id  = azurerm_public_ip.publicip3.id
 
   }
 }
 
-resource "azurerm_public_ip" "publicip1" {
-    name                         = "myPublicIP4"
+resource "azurerm_public_ip" "publicip3" {
+    name                         = "myPublicIP3"
     location                     = data.terraform_remote_state.remote.outputs.rglocation
     resource_group_name          = data.terraform_remote_state.remote.outputs.rgname
     allocation_method            = "Dynamic"
@@ -76,7 +76,7 @@ resource "azurerm_public_ip" "publicip1" {
 }
 
 data "azurerm_public_ip" "GitlabIp" {
-  name                = azurerm_public_ip.publicip4.name
+  name                = azurerm_public_ip.publicip3.name
   resource_group_name = azurerm_linux_virtual_machine.GitlabVm.resource_group_name
 }
 
